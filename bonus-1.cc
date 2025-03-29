1f78b94f7515f397295ec99fc5dd5bf36a3a6690
Peter Munch <peterrmuench@gmail.com>
2025-03-29 17:50:38 +0100

    Add missing  header

--- bonus-1.cc
+++ bonus-1.cc
@@ -1,3 +1,21 @@
+// ------------------------------------------------------------------------
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (C) 2021 - 2025 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// Part of the source code is dual licensed under Apache-2.0 WITH
+// LLVM-exception OR LGPL-2.1-or-later. Detailed license information
+// governing the source code and code contributions can be found in
+// LICENSE.md and CONTRIBUTING.md at the top level directory of deal.II.
+//
+// Author: Peter Munch, 2021, Helmholtz-Zentrum Hereon
+//         Peter Munch, 2025, Technical University Berlin
+//
+// Compute element mass matrix.
+//
+// ------------------------------------------------------------------------
 
 #include <deal.II/base/quadrature_lib.h>
 
